193c344e3e3c8b17e5c924e19b2de7d08e0626f4
Alex <baileymk@hotmail.com>
2024-04-08 11:55:35 +0100

    permission alert

--- src/components/userMarker/userMarker.tsx
+++ src/components/userMarker/userMarker.tsx
@@ -6,57 +6,88 @@ import location from "../../../public/images/location.webp";
 import "mapbox-gl/dist/mapbox-gl.css";
 import styles from "./userMarker.module.css";
 
+interface DeviceOrientationEventiOS extends DeviceOrientationEvent {
+  requestPermission?: () => Promise<"granted" | "denied">;
+}
+const requestPermission = (
+  DeviceOrientationEvent as unknown as DeviceOrientationEventiOS
+).requestPermission;
+const iOS = typeof requestPermission === "function";
 export const UserMarker = memo(function UserMarker() {
-    const viewState = useAppSelector((state) => state.map);
-    const [userLocation, setUserLocation] = useState<{
-        latitude: number;
-        longitude: number;
-    }>({ latitude: 0, longitude: 0 });
+  const viewState = useAppSelector((state) => state.map);
+  const [userLocation, setUserLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  }>({ latitude: 0, longitude: 0 });
 
-    useEffect(() => {
-        setLocation();
+  useEffect(() => {
+    setLocation();
 
-        window.addEventListener("devicemotion", (event) => {
-            if (event.acceleration?.x || event.acceleration?.y) {
-                alert(`${event.acceleration.x} " + " ${event.acceleration.y}`);
-            }
+    window.addEventListener("devicemotion", (event) => {
+      if (event.acceleration?.x || event.acceleration?.y) {
+        // alert(`${event.acceleration.x} " + " ${event.acceleration.y}`);
+      }
 
-            alert(event);
-        });
+      // alert(event);
+    });
 
-        if (window.DeviceMotionEvent) {
-            alert("device motion supported")
-        } else {
-            alert("device motion not supported")
-        }
-    }, []);
-
-    if (typeof window !== "undefined") {
-        window.onload = function() {
-            setLocation();
-        };
+    if (window.DeviceMotionEvent) {
+      // alert("device motion supported")
+    } else {
+      // alert("device motion not supported")
     }
 
-    function setLocation() {
-        console.log("setting user location");
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                setUserLocation({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
+    document
+      .querySelector(".mapboxgl-canvas")
+      ?.addEventListener("click", () => {
+        if (iOS) {
+          requestPermission()
+            .then((permissionState) => {
+              if (permissionState === "granted") {
+                window.addEventListener("devicemotion", () => {
+                  alert("permission granted");
                 });
-            });
+              }
+            })
+            .catch(console.error);
+        } else {
+          // handle regular non iOS 13+ devices
+          alert("not ios");
         }
-    }
+      });
+  }, []);
 
-    return (
-        <div className={styles.userMarker}>
-            <Marker
-                latitude={userLocation.latitude}
-                longitude={userLocation.longitude}
-            >
-                <Image src={location} alt={"location of user"} height={40} width={40} />
-            </Marker>
-        </div>
-    );
+  function handleDeviceMotion() {}
+
+  if (typeof window !== "undefined") {
+    window.onload = function () {
+      setLocation();
+    };
+    window.onfocus = function () {
+      setLocation();
+    };
+  }
+
+  function setLocation() {
+    console.log("setting user location");
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setUserLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+    }
+  }
+
+  return (
+    <div className={styles.userMarker}>
+      <Marker
+        latitude={userLocation.latitude}
+        longitude={userLocation.longitude}
+      >
+        <Image src={location} alt={"location of user"} height={40} width={40} />
+      </Marker>
+    </div>
+  );
 });
