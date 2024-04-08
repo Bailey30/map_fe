65b268f3bcd8aee895ef2df6fb096beb73bdcecc
Alex <baileymk@hotmail.com>
2024-04-08 11:40:10 +0100

    support alert

--- src/components/userMarker/userMarker.tsx
+++ src/components/userMarker/userMarker.tsx
@@ -7,50 +7,56 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import styles from "./userMarker.module.css";
 
 export const UserMarker = memo(function UserMarker() {
-  const viewState = useAppSelector((state) => state.map);
-  const [userLocation, setUserLocation] = useState<{
-    latitude: number;
-    longitude: number;
-  }>({ latitude: 0, longitude: 0 });
+    const viewState = useAppSelector((state) => state.map);
+    const [userLocation, setUserLocation] = useState<{
+        latitude: number;
+        longitude: number;
+    }>({ latitude: 0, longitude: 0 });
 
-  useEffect(() => {
-    setLocation();
+    useEffect(() => {
+        setLocation();
 
-    window.addEventListener("devicemotion", (event) => {
-      if (event.acceleration?.x || event.acceleration?.y) {
-        alert(`${event.acceleration.x} " + " ${event.acceleration.y}`);
-      }
+        window.addEventListener("devicemotion", (event) => {
+            if (event.acceleration?.x || event.acceleration?.y) {
+                alert(`${event.acceleration.x} " + " ${event.acceleration.y}`);
+            }
 
-      alert(event);
-    });
-  }, []);
-
-  if (typeof window !== "undefined") {
-    window.onload = function () {
-      setLocation();
-    };
-  }
-
-  function setLocation() {
-    console.log("setting user location");
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setUserLocation({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
+            alert(event);
         });
-      });
-    }
-  }
 
-  return (
-    <div className={styles.userMarker}>
-      <Marker
-        latitude={userLocation.latitude}
-        longitude={userLocation.longitude}
-      >
-        <Image src={location} alt={"location of user"} height={40} width={40} />
-      </Marker>
-    </div>
-  );
+        if (window.DeviceMotionEvent) {
+            alert("device motion supported")
+        } else {
+            alert("device motion not supported")
+        }
+    }, []);
+
+    if (typeof window !== "undefined") {
+        window.onload = function() {
+            setLocation();
+        };
+    }
+
+    function setLocation() {
+        console.log("setting user location");
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                setUserLocation({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+            });
+        }
+    }
+
+    return (
+        <div className={styles.userMarker}>
+            <Marker
+                latitude={userLocation.latitude}
+                longitude={userLocation.longitude}
+            >
+                <Image src={location} alt={"location of user"} height={40} width={40} />
+            </Marker>
+        </div>
+    );
 });
